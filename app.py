009ab9a6c4d8c571c093434c4225b179799c9e19
u-Shiyuh <xmatthewochoa@gmail.com>
2022-03-10 01:32:43 +0800

    versioning issue fix

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-import numpy as np
-import pandas as pd
+# import numpy as np
+# import pandas as pd
 from flask import Flask, render_template, url_for, request, jsonify
-import pickle
-import datetime
-import logging
+# import pickle
+# import datetime
+# import logging
 app = Flask(__name__)
 
 
 
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
+# app.logger.addHandler(logging.StreamHandler(sys.stdout))
+# app.logger.setLevel(logging.ERROR)
 
-filename_dtc = 'dtc.pkl'
-filename_gnb = 'gnb.pkl'
-filename_lr = 'lr.pkl'
-filename_rf = 'rf.pkl'
-filename_svm = 'svm.pkl'
-filename_scaler = 'scaler.pkl'
+# filename_dtc = 'dtc.pkl'
+# filename_gnb = 'gnb.pkl'
+# filename_lr = 'lr.pkl'
+# filename_rf = 'rf.pkl'
+# filename_svm = 'svm.pkl'
+# filename_scaler = 'scaler.pkl'
 # dtc = pickle.load(open(filename_dtc, 'rb'))
 # gnb = pickle.load(open(filename_gnb, 'rb'))
 # lr = pickle.load(open(filename_lr, 'rb'))
@@ -28,36 +28,36 @@ filename_scaler = 'scaler.pkl'
 def init():
     return render_template('index.html')
 
-@app.route('/predict_with_ajax', methods=['POST', 'GET'])
-def predict_with_ajax():
-#   if request.method == "POST":
-#     qtc_data = request.get_json()
-#     amount = qtc_data[0].get("amount")
-#     oldbalanceOrig = qtc_data[0].get('oldbalanceOrig')
-#     newbalanceOrig = qtc_data[0].get('newbalanceOrig')
-#     oldbalanceDest = qtc_data[0].get('oldbalanceDest')
-#     newbalanceDest = qtc_data[0].get('newbalanceDest')
-#     predict_val = pd.DataFrame(data = [
-#         [ datetime.datetime.now().hour,
-#         int(amount),
-#         int(newbalanceOrig),
-#         int(newbalanceDest),
-#         int(oldbalanceOrig) - (int(amount) + int(newbalanceOrig)),
-#         int(oldbalanceDest) - (int(newbalanceDest) + int(amount))]
-#     ], columns = ['step', 'amount','newbalanceOrig','newbalanceDest','originError','destError'])
-#     print(predict_val)
-#     predict_val = scaler.transform(predict_val)
-#     print(predict_val)
-#     return_value = {
-#         'dtc': int(dtc.predict(predict_val)[0]),
-#         'gnb': int(gnb.predict(predict_val)[0]),
-#         'lr': int(lr.predict(predict_val)[0]),
-#         'rf': int(rf.predict(predict_val)[0]),
-#         'svm' : int(svm.predict(predict_val)[0])
-#     }
+# @app.route('/predict_with_ajax', methods=['POST', 'GET'])
+# def predict_with_ajax():
+# #   if request.method == "POST":
+# #     qtc_data = request.get_json()
+# #     amount = qtc_data[0].get("amount")
+# #     oldbalanceOrig = qtc_data[0].get('oldbalanceOrig')
+# #     newbalanceOrig = qtc_data[0].get('newbalanceOrig')
+# #     oldbalanceDest = qtc_data[0].get('oldbalanceDest')
+# #     newbalanceDest = qtc_data[0].get('newbalanceDest')
+# #     predict_val = pd.DataFrame(data = [
+# #         [ datetime.datetime.now().hour,
+# #         int(amount),
+# #         int(newbalanceOrig),
+# #         int(newbalanceDest),
+# #         int(oldbalanceOrig) - (int(amount) + int(newbalanceOrig)),
+# #         int(oldbalanceDest) - (int(newbalanceDest) + int(amount))]
+# #     ], columns = ['step', 'amount','newbalanceOrig','newbalanceDest','originError','destError'])
+# #     print(predict_val)
+# #     predict_val = scaler.transform(predict_val)
+# #     print(predict_val)
+# #     return_value = {
+# #         'dtc': int(dtc.predict(predict_val)[0]),
+# #         'gnb': int(gnb.predict(predict_val)[0]),
+# #         'lr': int(lr.predict(predict_val)[0]),
+# #         'rf': int(rf.predict(predict_val)[0]),
+# #         'svm' : int(svm.predict(predict_val)[0])
+# #     }
 
-#     return jsonify(return_value)
-    return None
+# #     return jsonify(return_value)
+#     return None
 
 
 
